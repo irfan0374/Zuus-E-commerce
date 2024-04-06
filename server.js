91a8ed0c7839eee8aeb5264c79313f8e7e0f5461
Muhammed Irfan <130030879+irfan0374@users.noreply.github.com>
2024-04-06 15:24:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require('dotenv').config()
 const express = require('express')
 const session = require("express-session")
@@ -8,21 +8,7 @@ mongoose.connect(process.env.MONGO_URL).then((data) => console.log("Connected Mo
 
 const app = express();
 const cartCount = require('./middleware/cartcount')
-=======
 
-const express = require( 'express')
-const session=require("express-session")
-require('dotenv').config()
-const mongoose=require("mongoose");
-// mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
-mongoose.connect(process.env.MONGO_URL).then((data) => console.log("Connected MongoDb")).catch((err) => console.log(err))
-
-
-
-const app=express();
-
-const cartCount=require('./middleware/cartcount')
->>>>>>> 0bb788660fc170b6590cfe25469a19266d71a231
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');
   next();
@@ -30,17 +16,11 @@ app.use((req, res, next) => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(session({
-<<<<<<< HEAD
   secret: "secretKey",
   resave: false,
   saveUninitialized: false,
 }));
-=======
-    secret: "SECRETKEY", 
-    resave: false,              
-    saveUninitialized: false,   
-  }));
->>>>>>> 0bb788660fc170b6590cfe25469a19266d71a231
+
 app.use(cartCount.cartcount)
 
 app.set('view engine', 'ejs');
@@ -62,12 +42,8 @@ app.use((req, res) => {
 })
 
 
-<<<<<<< HEAD
+
 app.listen(4000, () => {
   console.log('running sucessfull');
 }) 
-=======
-app.listen(4000 , () => {
-    console.log('running sucessfull');
-}) 
->>>>>>> 0bb788660fc170b6590cfe25469a19266d71a231
+
