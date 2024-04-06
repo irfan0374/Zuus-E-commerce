35d8f50d0c202e21b740481071bb19147bb69eca
Irfan <irfan188iqbal@gmail.com>
2024-04-06 15:16:50 +0530

    updated

--- server.js
+++ server.js
@@ -1,48 +1,44 @@
-const mongoose=require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const session = require("express-session")
+const mongoose = require("mongoose");
 // mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
-mongoose.connect("mongodb+srv://zuus:xOocmS8ag8Qevvjf@cluster0.czkonjb.mongodb.net/e-commerce").then((data) => console.log("Connected MongoDb")).catch((err) => console.log(err))
+mongoose.connect(process.env.MONGO_URL).then((data) => console.log("Connected MongoDb")).catch((err) => console.log(err))
 
-
-
-
-const express = require( 'express')
-const session=require("express-session")
-const dotenv=require('dotenv').config()
-
-const app=express();
-const cartCount=require('./middleware/cartcount')
+const app = express();
+const cartCount = require('./middleware/cartcount')
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');
   next();
 });
-app.use( express.json())
-app.use( express.urlencoded({extended : false}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(session({
-    secret: process.env.secret, 
-    resave: false,              
-    saveUninitialized: false,   
-  }));
+  secret: "secretKey",
+  resave: false,
+  saveUninitialized: false,
+}));
 app.use(cartCount.cartcount)
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 
-const userroute=require('./route/userroute')
-app.use('/',userroute)
+const userroute = require('./route/userroute')
+app.use('/', userroute)
 
-const adminroute=require('./route/adminroute')
-app.use('/admin',adminroute)
+const adminroute = require('./route/adminroute')
+app.use('/admin', adminroute)
 
-app.use((req,res)=>{
-  try{
+app.use((req, res) => {
+  try {
     res.redirect('/error')
-  }catch(error){
+  } catch (error) {
     console.log(error.message)
   }
 })
 
 
-app.listen(4000 , () => {
-    console.log('running sucessfull');
+app.listen(4000, () => {
+  console.log('running sucessfull');
 }) 
